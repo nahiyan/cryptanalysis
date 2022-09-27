4b1bd99c4834a3e63caddf5d6ce9abe9760cd371
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2022-09-27 17:20:50 -0400

    Rename script name to be more specific

--- encodings-generator-saeed.py
+++ encodings-generator-saeed.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 # Goal 1: Generate the encoding with and without XOR clauses
 # Goal 2: Try variants with the adders: counter_chain and dot_matrix
@@ -14,11 +15,17 @@ hashes = ["f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f",
 adder_types = ["counter_chain", "dot_matrix"]
 step_variations = list(range(16, 49))
 
+# Check if the executable exists
+encoder_path = "./encoders/saeed/crypto/main"
+if not os.path.exists(encoder_path):
+    sys.exit("Failed to find the encoder in the 'encoders/saeed/crypto' directory. Can you ensure that you compiled it?")
+
 for xor_option in xor_options:
     xor_flag = xor_option
     for adder_type in adder_types:
         for steps in step_variations:
-            os.system("./encoders/ {} -A {} -r {} -f md4 -a preimage > encodings/saeed/md4_{}_{}_xor{}.cnf".format(
+            os.system("{} {} -A {} -r {} -f md4 -a preimage > encodings/saeed/md4_{}_{}_xor{}.cnf".format(
+                encoder_path,
                 "--xor" if xor_flag else None,
                 adder_type,
                 steps,
