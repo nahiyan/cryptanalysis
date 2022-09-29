31b62f4cd3fc211ccbc5d4b5d9c9fd08e42cde3d
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2022-09-29 11:33:15 -0400

    Write a tool to analyze SAT solutions

---
+++ scripts/solution_analyzer/src/main.rs
@@ -0,0 +1,97 @@
+use std::collections::btree_map::Range;
+use std::env;
+use std::fs;
+use std::str::Bytes;
+use std::str::FromStr;
+use std::vec;
+
+struct Context<'a> {
+    variable_ranges: &'a mut Vec<(u32, u32)>,
+    variable_bytes: &'a mut Vec<u8>,
+}
+
+impl Context<'_> {
+    fn analyze_line(&mut self, line: &str) {
+        // Ensure that the line starts with 'v'
+        if !line.starts_with('v') {
+            return;
+        }
+
+        let pieces = line.split_whitespace();
+        for piece in pieces {
+            if !piece.eq("v") && !piece.eq("0") {
+                let variable: u32 = i32::unsigned_abs(FromStr::from_str(piece).unwrap());
+                let value: u8 = if piece.starts_with('-') { 0 } else { 1 };
+
+                for (start, end) in self.variable_ranges.iter() {
+                    if variable >= *start && variable <= *end {
+                        self.variable_bytes.push(value)
+                    }
+                }
+            }
+        }
+    }
+
+    fn analyze(&mut self, content: String) {
+        let mut line: String = String::from("");
+        for c in content.chars() {
+            // End of line
+            if c == '\n' {
+                self.analyze_line(line.as_str());
+                line = String::from("")
+            } else {
+                line.push(c)
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut context = Context {
+        variable_bytes: &mut vec![],
+        variable_ranges: &mut vec![],
+    };
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        panic!("Missing argument: The solutions file, which holds the output of the SAT solver.");
+    }
+
+    if args.len() > 2 {
+        for arg in &args[2..] {
+            let pieces = arg.split('-');
+            if pieces.clone().count() != 2 {
+                panic!("Invalid variable range provided as as argument");
+            }
+
+            let pieces_int: Vec<u32> = pieces.map(|x| FromStr::from_str(x).unwrap()).collect();
+
+            let start = pieces_int[0];
+            let end = pieces_int[1];
+
+            // if (end - start + 1) % 8 != 0 {
+            //     panic!("One of the ranges has a width not divisible by 8!")
+            // }
+
+            context.variable_ranges.push((start, end));
+        }
+    }
+
+    let encodings_file = &args[1];
+    match fs::read_to_string(encodings_file) {
+        Err(_) => {
+            println!("Failed to open the solutions file.");
+        }
+        Ok(content) => {
+            context.analyze(content);
+        }
+    }
+
+    let mut i = 0;
+    for (start, end) in context.variable_ranges.iter() {
+        for j in *start..*end + 1 {
+            println!("{} -> {}", j, context.variable_bytes[i]);
+            i += 1
+        }
+    }
+}
