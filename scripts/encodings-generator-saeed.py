c95fe733e4102acec457263476a869a3d4edcff3
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2022-09-28 21:45:52 -0400

    Move the scripts to their own directory

---
+++ scripts/encodings-generator-saeed.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+# Goal 1: Generate the encoding with and without XOR clauses
+# Goal 2: Try variants with the adders: counter_chain and dot_matrix
+# Goal 3: Try various rounds from 16-48
+# TODO: Goal 4: Try various hashes for the inversion
+# TODO: Add options to pick the encoder: Saeed, Oleg, etc.
+
+xor_options = [True, False]
+
+# TODO: Implement passing the hashes to the encoder
+hashes = ["ffffffffffffffffffffffffffffffff",
+          "00000000000000000000000000000000"]
+adder_types = ["counter_chain", "dot_matrix"]
+step_variations = list(range(16, 49))
+
+# Check if the executable exists
+encoder_path = "encoders/saeed/crypto/main"
+if not os.path.exists(encoder_path):
+    sys.exit("Failed to find the encoder in the 'encoders/saeed/crypto' directory. Can you ensure that you compiled it?")
+
+for hash in hashes:
+    for xor_option in xor_options:
+        xor_flag = xor_option
+        for adder_type in adder_types:
+            for steps in step_variations:
+                os.system("{} {} -A {} -r {} -f md4 -a preimage -t {} > encodings/saeed/md4_{}_{}_xor{}_{}.cnf".format(
+                    encoder_path,
+                    "--xor" if xor_flag else None,
+                    adder_type,
+                    steps,
+                    hash,
+                    steps,
+                    adder_type,
+                    "1" if xor_flag == True else "0",
+                    hash
+                ))
