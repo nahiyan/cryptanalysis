5c95918bc5b0428ef84d09301a3cba154bdc6752
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2022-11-07 10:11:55 -0500

    Add logic to automatically handle job allocations with triggers

--- tools/benchmark/slurm/slurm.go
+++ tools/benchmark/slurm/slurm.go
@@ -1,15 +1,23 @@
 package slurm
 
 import (
+	"benchmark/config"
 	"benchmark/constants"
+	"benchmark/db"
 	"benchmark/encodings"
 	"benchmark/types"
 	"benchmark/utils"
+	"bytes"
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
 	"os"
 	"os/exec"
+	"path"
+
+	"github.com/samber/lo"
+	"gorm.io/gorm"
 )
 
 func writeSlurmJob(job types.SlurmJob, satSolver, instanceName string) (string, error) {
@@ -59,7 +67,110 @@ func generateJobs(context *types.CommandContext) []string {
 	return filePaths
 }
 
+func getScheduledJobCount() uint {
+	output, err := exec.Command("sq").Output()
+	if err != nil {
+		return 0
+	}
+
+	reader := bytes.NewReader(output)
+	lines, err := utils.CountLines(reader)
+	if err != nil {
+		return 0
+	}
+
+	return uint(lines - 1)
+}
+
+func schedule(tx *gorm.DB, jobFilePaths []string) error {
+	areAllJobsScheduled := true
+
+	// Use DB transaction to lock the database
+	if err := tx.Transaction(func(_ *gorm.DB) error {
+		for _, jobFilePath := range jobFilePaths {
+			// Schedule
+			cmd := exec.Command("sbatch", jobFilePath)
+			output_, err := cmd.Output()
+			output := string(output_)
+			fmt.Print(output)
+			if err != nil {
+				areAllJobsScheduled = false
+				// Commit the transaction
+				return nil
+			}
+
+			// Grab the job ID
+			jobId := utils.GetJobId(output)
+
+			// Schedule trigger upon completion
+			if err := exec.Command(fmt.Sprintf("strigger --set --jobid=%d --fini --program=\"%s slurm --digest %d\"", jobId, config.Get().Paths.Bin.Benchmark, jobId)).Run(); err != nil {
+				return errors.New("failed to set strigger")
+			}
+
+			// Register the Slurm ID in the database
+			if err := tx.Model(&types.Job{}).Where("file_name", path.Base(jobFilePath)).Update("slurm_id", jobId).Error; err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	if !areAllJobsScheduled {
+		return errors.New(constants.ErrOneJobScheduleFailed)
+	}
+
+	return nil
+}
+
+func scheduleToLimit() error {
+	if err := db.Get().Transaction(func(tx *gorm.DB) error {
+		emptySpots := config.Get().Slurm.MaxJobs - getScheduledJobCount()
+		if emptySpots > 0 {
+			pendingJobs := make([]types.Job, 0)
+
+			if err := tx.Limit(int(emptySpots)).Find(pendingJobs).Error; err != nil {
+				return err
+			}
+
+			pendingJobFilePaths := lo.Map(pendingJobs, func(job types.Job, i int) string {
+				return fmt.Sprintf("%s%s", constants.JobsDirPath, job.FileName)
+			})
+			if err := schedule(tx, pendingJobFilePaths); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}); err != nil {
+		// Commit transaction even if one of the jobs failed
+		if err.Error() == constants.ErrOneJobScheduleFailed {
+			return nil
+		}
+
+		return err
+	}
+
+	return nil
+}
+
 func Run(context *types.CommandContext) {
+	// Handle any job that needs to be digested
+	if context.Digest != 0 {
+		if err := db.Get().Where("slurm_id", context.Digest).Delete(&types.Job{}).Error; err != nil {
+			log.Fatal(err)
+		}
+
+		// Schedule more jobs
+		if err := scheduleToLimit(); err != nil {
+			log.Fatal(err)
+		}
+
+		return
+	}
+
 	// Generate encodings
 	encodings.Generate(types.EncodingsGenContext{
 		Variations:    context.Variations,
@@ -70,13 +181,15 @@ func Run(context *types.CommandContext) {
 	// Generate jobs
 	jobFilePaths := generateJobs(context)
 
-	// Schedule the jobs
-	for _, jobFilePath := range jobFilePaths {
-		cmd := exec.Command("sbatch", jobFilePath)
-		output, err := cmd.Output()
-		fmt.Print(string(output))
-		if err != nil {
-			fmt.Println("Job schedule failed:", jobFilePath)
+	// Add to the database
+	db.Get().Create(lo.Map(jobFilePaths, func(filePath string, i int) *types.Job {
+		return &types.Job{
+			FileName: path.Base(filePath),
 		}
+	}))
+
+	// Schedule the jobs up to the limit
+	if err := scheduleToLimit(); err != nil {
+		log.Fatal(err)
 	}
 }
