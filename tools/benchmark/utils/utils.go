1e929f9497cb000d307089842f3c4806684dacd2
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2022-11-08 18:10:20 -0500

    Add util function for reading a specific line in a file

--- tools/benchmark/utils/utils.go
+++ tools/benchmark/utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"benchmark/constants"
 	"benchmark/types"
+	"bufio"
 	"bytes"
 	"crypto/sha1"
 	"encoding/csv"
@@ -234,4 +235,16 @@ func GetJobId(output string) int {
 		return 0
 	}
 	return jobId
+}
+
+func ReadLine(r io.Reader, lineNum int) (line string, lastLine int, err error) {
+	sc := bufio.NewScanner(r)
+	for sc.Scan() {
+		lastLine++
+		if lastLine == lineNum {
+			// you can return sc.Bytes() if you need output in []bytes
+			return sc.Text(), lastLine, sc.Err()
+		}
+	}
+	return line, lastLine, io.EOF
 }
