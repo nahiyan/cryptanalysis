25b4063df6aad636601cea543ca8d4eb539fbfda
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2022-11-28 09:59:48 -0500

    Add function to generate and return the sub-problem as a string

--- tools/benchmark/encodings/cubes.go
+++ tools/benchmark/encodings/cubes.go
@@ -13,7 +13,60 @@ import (
 	"github.com/samber/lo"
 )
 
-func GenerateSubProblem(instanceName string, i int) error {
+func GenerateSubProblemAsStringWithThreshold(instanceName string, i int, threshold *uint) (string, error) {
+	subProblemFilePath := fmt.Sprintf("%scube%d_%s.cnf", constants.EncodingsDirPath, i, instanceName)
+	if utils.FileExists(subProblemFilePath) {
+		return "", nil
+	}
+
+	// * 1. Read the instance
+	instance_, err := os.ReadFile(fmt.Sprintf("%s%s.cnf", constants.EncodingsDirPath, instanceName))
+	if err != nil {
+		return "", err
+	}
+	instance := string(instance_)
+
+	// * 2. Open the cubes file
+	cubesFilePath := func(threshold *uint) string {
+		if threshold == nil {
+			return fmt.Sprintf("%s%s.icnf", constants.EncodingsDirPath, instanceName)
+		}
+		return fmt.Sprintf("%sn%d_%s.icnf", constants.EncodingsDirPath, *threshold, instanceName)
+	}(threshold)
+	cubesFile, err := os.Open(cubesFilePath)
+	if err != nil {
+		return "", err
+	}
+	defer cubesFile.Close()
+
+	// * 3. Get the cube
+	cube, _, _ := utils.ReadLine(cubesFile, i)
+
+	// * 4. Generate unit clauses from the literals in the cube
+	cubeClauses_ := strings.Split(strings.TrimPrefix(cube, "a "), " ")
+	cubeClauses := lo.Map(cubeClauses_[:len(cubeClauses_)-1], func(s string, _ int) string {
+		return s + " 0"
+	})
+
+	// * 5. Get the num. of variables and clauses along with the body
+	var numVars, numClauses int
+	fmt.Sscanf(instance, "p cnf %d %d\n", &numVars, &numClauses)
+	n := len(fmt.Sprintf("p cnf %d %d\n", numVars, numClauses))
+	body := instance[n:]
+
+	// * 6. Generate a new header with an increased number of clauses
+	newHeader := fmt.Sprintf("p cnf %d %d", numVars, numClauses+len(cubeClauses))
+
+	// * 7. Assemble the new encoding
+	newEncoding := fmt.Sprintf("%s\n%s%s\n",
+		newHeader,
+		body,
+		strings.Join(cubeClauses, "\n"))
+
+	return newEncoding, nil
+}
+
+func GenerateSubProblemWithThreshold(instanceName string, i int, threshold *uint) error {
 	subProblemFilePath := fmt.Sprintf("%scube%d_%s.cnf", constants.EncodingsDirPath, i, instanceName)
 	if utils.FileExists(subProblemFilePath) {
 		return nil
@@ -27,7 +80,13 @@ func GenerateSubProblem(instanceName string, i int) error {
 	instance := string(instance_)
 
 	// * 2. Open the cubes file
-	cubesFile, err := os.Open(fmt.Sprintf("%s%s.icnf", constants.EncodingsDirPath, instanceName))
+	cubesFilePath := func(threshold *uint) string {
+		if threshold == nil {
+			return fmt.Sprintf("%s%s.icnf", constants.EncodingsDirPath, instanceName)
+		}
+		return fmt.Sprintf("%sn%d_%s.icnf", constants.EncodingsDirPath, *threshold, instanceName)
+	}(threshold)
+	cubesFile, err := os.Open(cubesFilePath)
 	if err != nil {
 		return err
 	}
@@ -58,12 +117,21 @@ func GenerateSubProblem(instanceName string, i int) error {
 	}
 	defer subProblemFile.Close()
 
-	if _, err := subProblemFile.WriteString(
-		fmt.Sprintf("%s\n%s%s\n",
-			newHeader,
-			body,
-			strings.Join(cubeClauses, "\n"))); err != nil {
+	newEncoding := fmt.Sprintf("%s\n%s%s\n",
+		newHeader,
+		body,
+		strings.Join(cubeClauses, "\n"))
+
+	if _, err := subProblemFile.WriteString(newEncoding); err != nil {
 		return errors.New("failed to add the clauses to the subproblem file")
+	}
+
+	return nil
+}
+
+func GenerateSubProblem(instanceName string, i int) error {
+	if err := GenerateSubProblemWithThreshold(instanceName, i, nil); err != nil {
+		return err
 	}
 
 	return nil
