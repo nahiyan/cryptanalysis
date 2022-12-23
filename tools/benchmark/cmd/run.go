5d302bec011c10a6fb765c80c0bd0a6e88312e25
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2022-12-23 18:19:31 -0500

    Make the "run" command working

--- tools/benchmark/cmd/run.go
+++ tools/benchmark/cmd/run.go
@@ -2,7 +2,7 @@ package cmd
 
 import (
 	"benchmark/internal/container"
-	"benchmark/internal/schema/services"
+	schemaServices "benchmark/internal/schema/services"
 
 	"github.com/samber/do"
 	"github.com/spf13/cobra"
@@ -16,8 +16,8 @@ func initRunCmd() *cobra.Command {
 		Short: "Run the benchmark based on the defined pipeline",
 		Run: func(cmd *cobra.Command, args []string) {
 			injector := container.InitInjector()
-			schemaService := do.MustInvoke[*services.SchemaService](injector)
-			schemaService.Process(schemaFilePath)
+			schemaSvc := do.MustInvoke[*schemaServices.SchemaService](injector)
+			schemaSvc.Process(schemaFilePath)
 		},
 	}
 
