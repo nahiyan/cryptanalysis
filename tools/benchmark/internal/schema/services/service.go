c8232fbf1293cc9527b523b87d78ea59e06b7ede
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2022-12-23 17:10:53 -0500

    Update schema module

--- tools/benchmark/internal/schema/services/service.go
+++ tools/benchmark/internal/schema/services/service.go
@@ -18,27 +18,6 @@ func NewSchemaService(i *do.Injector) (*SchemaService, error) {
 }
 
 func (c *SchemaService) Process(filePath string) {
-	// benchmarkBinAbsPath, err := filepath.Abs("./benchmark")
-	// if err != nil {
-	// 	benchmarkBinAbsPath = "./benchmark"
-	// }
-
-	// // Binaries
-	// viper.SetDefault("Paths.Bin.CryptoMiniSat", "../../../sat-solvers/cryptominisat")
-	// viper.SetDefault("Paths.Bin.Kissat", "../../../sat-solvers/kissat")
-	// viper.SetDefault("Paths.Bin.Cadical", "../../../sat-solvers/cadical")
-	// viper.SetDefault("Paths.Bin.Glucose", "../../../sat-solvers/glucose")
-	// viper.SetDefault("Paths.Bin.MapleSat", "../../../sat-solvers/maplesat")
-	// viper.SetDefault("Paths.Bin.XnfSat", "../../../sat-solvers/xnfsat")
-	// viper.SetDefault("Paths.Bin.March", "../../../sat-solvers/march_cu")
-	// viper.SetDefault("Paths.Bin.SolutionAnalyzer", "../solution_analyzer/target/release/solution_analyzer")
-	// viper.SetDefault("Paths.Bin.Encoder", "../../encoders/saeed/crypto/main")
-	// viper.SetDefault("Paths.Bin.Verifier", "../../encoders/saeed/crypto/verify-md4")
-	// viper.SetDefault("Paths.Bin.Benchmark", benchmarkBinAbsPath)
-
-	// Slurm
-	// viper.SetDefault("Slurm.MaxJobs", 1000)
-
 	// Set config file
 	viper.SetConfigFile(filePath)
 	if err := viper.ReadInConfig(); err != nil {
