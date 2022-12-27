ad29902ed02c0af26032523a43024e70b236e05a
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2022-12-27 17:18:29 -0500

    Add constants

---
+++ tools/benchmark/internal/consts/consts.go
@@ -0,0 +1,21 @@
+package consts
+
+const (
+	CryptoMiniSat = "cryptominisat"
+	Cadical       = "cadical"
+	Kissat        = "kissat"
+	MapleSat      = "maplesat"
+	Glucose       = "glucose"
+	XnfSat        = "xnfsat"
+)
+
+const (
+	Regular = iota
+	Slurm
+)
+
+const (
+	Sat = iota
+	Unsat
+	Fail
+)
