3b25fad49bd8aa7390c19a547efd9209dd35c2b2
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2023-01-01 17:06:07 -0500

    Add encoding module

---
+++ tools/benchmark/internal/encoding/services/service.go
@@ -0,0 +1,80 @@
+package services
+
+import (
+	"bufio"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/samber/lo"
+)
+
+func (encodingSvc *EncodingService) Process(encoding string) (int, int, error) {
+	clauses := 0
+	freeVariables := 0
+
+	instanceFile, err := os.Open(encoding)
+	if err != nil {
+		return 0, 0, err
+	}
+	defer instanceFile.Close()
+
+	variables := []uint{}
+	scanner := bufio.NewScanner(instanceFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// Check if it's the header
+		if strings.HasPrefix(line, "p cnf ") {
+			continue
+		}
+
+		// Check if it's a comment
+		if strings.HasPrefix(line, "c ") {
+			continue
+		}
+
+		// Divide the line into segments
+		segments := strings.Fields(line)
+
+		// Ignore an empty clause
+		if len(segments) < 2 {
+			continue
+		}
+
+		// Check if the clause is zero-terminated
+		if lastSegment, err := lo.Last(segments); err != nil || strings.TrimSpace(lastSegment) != "0" {
+			continue
+		}
+
+		// Parse the literals
+		literals_ := lo.Map(segments, func(s string, i int) int {
+			v, err := strconv.Atoi(s)
+			if err != nil {
+				return 0
+			}
+
+			return v
+		})
+		literals := lo.Filter(literals_, func(i1, i2 int) bool {
+			return i1 != 0
+		})
+
+		for _, literal := range literals {
+			variable := uint(int(math.Abs(float64(literal))))
+			// New variable
+			if !lo.Contains(variables, variable) {
+				variables = append(variables, uint(variable))
+				freeVariables += 1
+			}
+		}
+
+		clauses += 1
+	}
+
+	if err := scanner.Err(); err != nil {
+		return 0, 0, err
+	}
+
+	return freeVariables, clauses, nil
+}
