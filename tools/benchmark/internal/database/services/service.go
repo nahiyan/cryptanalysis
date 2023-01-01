a7465bd2066aa0ae1e1f28fcae3247124208fefc
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2023-01-01 17:05:29 -0500

    Add database bucket for cubesets

--- tools/benchmark/internal/database/services/service.go
+++ tools/benchmark/internal/database/services/service.go
@@ -13,7 +13,7 @@ type Properties struct {
 }
 
 func (databaseSvc *DatabaseService) CreateBuckets() error {
-	buckets := []string{"solutions", "tasks"}
+	buckets := []string{"solutions", "tasks", "cubesets"}
 
 	err := databaseSvc.db.Update(func(tx *bolt.Tx) error {
 		for _, bucket := range buckets {
