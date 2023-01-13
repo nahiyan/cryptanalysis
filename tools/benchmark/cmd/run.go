8aa7e1998f634ad61e76ce6a53a79998c9422fbb
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2023-01-13 12:11:46 -0500

    Take the schema file path as a positional argument

--- tools/benchmark/cmd/run.go
+++ tools/benchmark/cmd/run.go
@@ -12,16 +12,18 @@ func initRunCmd() *cobra.Command {
 	schemaFilePath := ""
 
 	cmd := &cobra.Command{
-		Use:   "run",
+		Use:   "run [flags] [schema_file_path]",
 		Short: "Run the benchmark based on the defined pipeline",
+		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
+			schemaFilePath = args[0]
 			injector := injector.New()
 			schemaSvc := do.MustInvoke[*schemaServices.SchemaService](injector)
 			schemaSvc.Process(schemaFilePath)
 		},
 	}
 
-	cmd.Flags().StringVarP(&schemaFilePath, "schema", "s", "schema.toml", "A schema is a TOML file that holds the pipelines for the benchmark")
+	// cmd.Flags().StringVarP(&schemaFilePath, "schema", "s", "schema.toml", "A schema is a TOML file that holds the pipelines for the benchmark")
 
 	return cmd
 }
