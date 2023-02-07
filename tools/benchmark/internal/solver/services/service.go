57af59d4f332a4841a2f5fe3a79d908a12567044
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2023-02-07 17:12:08 -0500

    Improve the solver module

--- tools/benchmark/internal/solver/services/service.go
+++ tools/benchmark/internal/solver/services/service.go
@@ -1,11 +1,8 @@
 package services
 
 import (
-	"benchmark/internal/consts"
-	"benchmark/internal/encoder"
 	errorModule "benchmark/internal/error"
 	"benchmark/internal/pipeline"
-	"benchmark/internal/simplifier"
 	"benchmark/internal/slurm"
 	solveslurmtask "benchmark/internal/solve_slurm_task"
 	"benchmark/internal/solver"
@@ -13,42 +10,41 @@ import (
 	"fmt"
 	"log"
 	"math"
-	"os"
 	"os/exec"
 	"path"
 	"strings"
 	"time"
 
 	"github.com/alitto/pond"
-	"github.com/samber/mo"
 	"github.com/sirupsen/logrus"
 )
 
-func (solverSvc *SolverService) GetCmdInfo(encoding string, solver solver.Solver, solutionPath string) (string, []string) {
+// TODO: Refactor
+func (solverSvc *SolverService) GetCmdInfo(encoding string, solver_ solver.Solver, solutionPath string) (string, []string) {
 	config := solverSvc.configSvc.Config
 
 	var binPath string
-	var args string
-	switch solver {
-	case consts.Kissat:
+	args := encoding
+	switch solver_ {
+	case solver.Kissat:
 		binPath = config.Paths.Bin.Kissat
-		args = "-q"
-	case consts.Cadical:
+		// args = "-q"
+	case solver.Cadical:
 		binPath = config.Paths.Bin.Cadical
-		args = "-q"
-	case consts.CryptoMiniSat:
+		// args = "-q"
+	case solver.CryptoMiniSat:
 		binPath = config.Paths.Bin.CryptoMiniSat
-		args = "--verb=0"
-	case consts.MapleSat:
+		// args = "--verb=0"
+	case solver.MapleSat:
 		binPath = config.Paths.Bin.MapleSat
-		args = "-verb=0"
-	case consts.Glucose:
+		// args = "-verb=0"
+	case solver.Glucose:
 		binPath = config.Paths.Bin.Glucose
-		args = "-verb=0"
+		// args = "-verb=0"
 	}
 
-	args += " " + encoding
-	if solver == consts.MapleSat || solver == consts.Glucose {
+	// args += " " + encoding
+	if solver_ == solver.MapleSat || solver_ == solver.Glucose {
 		args += " " + solutionPath
 	}
 	args_ := strings.Fields(args)
@@ -56,33 +52,35 @@ func (solverSvc *SolverService) GetCmdInfo(encoding string, solver solver.Solver
 	return binPath, args_
 }
 
-func (solverSvc *SolverService) Invoke(encoding string, solver_ solver.Solver, timeout int) (string, time.Duration, solver.Result, int) {
+func (solverSvc *SolverService) Invoke(encoding string, solver_ solver.Solver, timeout int) (time.Duration, solver.Result, int) {
 	errorSvc := solverSvc.errorSvc
-	solutionPath := path.Join("./tmp", path.Base(encoding)+"."+string(solver_)+".sol")
+	solutionsDir := solverSvc.configSvc.Config.Paths.Solutions
+	solutionPath := path.Join(solutionsDir, path.Base(encoding)+"."+string(solver_)+".sol")
 	binPath, solverArgs := solverSvc.GetCmdInfo(encoding, solver_, solutionPath)
 	duration := time.Duration(timeout) * time.Second
-	var (
-		result   solver.Result = consts.Fail
-		exitCode int
-	)
 
 	ctx, cancel := context.WithTimeout(context.Background(), duration)
 	defer cancel()
 
-	// Run and handle result
+	// Run and handle the result
 	cmd := exec.CommandContext(ctx, binPath, solverArgs...)
 	pipe, err := cmd.StdoutPipe()
 	solverSvc.errorSvc.Fatal(err, "Solver: failed to open pipe")
 	startTime := time.Now()
 	cmd.Start()
 
-	if solver_ != consts.MapleSat && solver_ != consts.Glucose {
-		err = solverSvc.filesystemSvc.WriteFromPipe(pipe, solutionPath)
-		solverSvc.errorSvc.Fatal(err, "Solver: failed to write from pipe")
-	}
+	// Write from stdout pipe to log file
+	logsDir := solverSvc.configSvc.Config.Paths.Logs
+	logFilePath := path.Join(logsDir, path.Base(encoding)+"."+string(solver_)+".log")
+	err = solverSvc.filesystemSvc.WriteFromPipe(pipe, logFilePath)
+	solverSvc.errorSvc.Fatal(err, "Solver: failed to write from pipe")
 
 	err = cmd.Wait()
 	runtime := time.Since(startTime)
+	var (
+		result   solver.Result = solver.Fail
+		exitCode int
+	)
 	errorSvc.Handle(err, func(err error) {
 		exiterr, ok := err.(*exec.ExitError)
 		if !ok {
@@ -91,95 +89,92 @@ func (solverSvc *SolverService) Invoke(encoding string, solver_ solver.Solver, t
 
 		exitCode = exiterr.ExitCode()
 		if exitCode == 10 {
-			result = consts.Sat
+			result = solver.Sat
 		} else if exitCode == 20 {
-			result = consts.Unsat
+			result = solver.Unsat
 		} else {
 			logrus.Error(err)
 		}
 	})
 
-	return solutionPath, runtime, result, exitCode
+	return runtime, result, exitCode
 }
 
 func (solverSvc *SolverService) TrackedInvoke(encoding string, solver_ solver.Solver, timeout int) {
-	solutionSvc := solverSvc.solutionSvc
+	// solutionSvc := solverSvc.solutionSvc
 
 	// Invoke
-	solutionPath, runtime, result, exitCode := solverSvc.Invoke(encoding, solver_, timeout)
+	runtime, result, exitCode := solverSvc.Invoke(encoding, solver_, timeout)
 	runtime = runtime.Round(time.Millisecond)
 
-	resultString := "Fail"
-	if result == consts.Sat {
-		resultString = "SAT"
-	} else if result == consts.Unsat {
-		resultString = "UNSAT"
-	}
+	// verified := false
+	// checksum := ""
+	// if result == solver.Sat {
+	// 	err := solutionSvc.FromLog(logFilePath, solver_)
+	// 	solverSvc.errorSvc.Fatal(err, "Solver: failed to extract solution from log")
 
-	verified := false
-	checksum := ""
-	if result == consts.Sat {
-		err := solutionSvc.Normalize(solutionPath)
-		solverSvc.errorSvc.Fatal(err, "Solver: failed to normalize solution")
-		info, err := solverSvc.encoderSvc.ProcessInstanceName(encoding)
-		solverSvc.errorSvc.Fatal(err, "Solver: failed to process instance name")
+	// 	err = solutionSvc.Normalize(solutionPath)
+	// 	solverSvc.errorSvc.Fatal(err, "Solver: failed to normalize solution")
+	// 	info, err := solverSvc.encoderSvc.ProcessInstanceName(encoding)
+	// 	solverSvc.errorSvc.Fatal(err, "Solver: failed to process instance name")
 
-		needsReconstruction := false
-		needsRemapping := false
-		{
-			if simplificationInfo, exists := info.Simplification.Get(); exists {
-				needsReconstruction = simplificationInfo.Simplifier == simplifier.Cadical
-				needsRemapping = simplificationInfo.Simplifier == simplifier.Satelite
-			}
-		}
+	// 	needsReconstruction := false
+	// 	needsRemapping := false
+	// 	{
+	// 		if simplificationInfo, exists := info.Simplification.Get(); exists {
+	// 			needsReconstruction = simplificationInfo.Simplifier == simplifier.Cadical
+	// 			needsRemapping = simplificationInfo.Simplifier == simplifier.Satelite
+	// 		}
+	// 	}
 
-		if needsReconstruction {
-			info.Cubing = mo.None[encoder.CubingInfo]()
-			info.CubeIndex = mo.None[int]()
-			instance := path.Join(path.Dir(encoding), solverSvc.encoderSvc.GetInstanceName(info))
-			rsPath := instance + ".rs.txt"
-			solutionSvc.Reconstruct(solutionPath, rsPath, encoding)
-			solverSvc.errorSvc.Fatal(err, "Solver: failed to reconstruct solution")
-		} else if needsRemapping {
-			info.Cubing = mo.None[encoder.CubingInfo]()
-			info.CubeIndex = mo.None[int]()
-			instance := path.Join(path.Dir(encoding), "..", solverSvc.encoderSvc.GetInstanceName(info))
-			varMapPath := instance + ".var_map.txt"
-			err := solutionSvc.Remap(solutionPath, varMapPath)
-			solverSvc.errorSvc.Fatal(err, "Solver: failed to remap variables in the solution")
-		}
+	// 	if needsReconstruction {
+	// 		// TODO: Fix the issue when the instance is a cube
+	// 		info.Cubing = mo.None[encoder.CubingInfo]()
+	// 		info.CubeIndex = mo.None[int]()
+	// 		instance := path.Join(path.Dir(encoding), solverSvc.encoderSvc.GetInstanceName(info))
+	// 		rsPath := instance + ".rs.txt"
+	// 		solutionSvc.Reconstruct(solutionPath, rsPath, encoding)
+	// 		solverSvc.errorSvc.Fatal(err, "Solver: failed to reconstruct solution")
+	// 	} else if needsRemapping {
+	// 		info.Cubing = mo.None[encoder.CubingInfo]()
+	// 		info.CubeIndex = mo.None[int]()
+	// 		instance := path.Join(path.Dir(encoding), "..", solverSvc.encoderSvc.GetInstanceName(info))
+	// 		varMapPath := instance + ".var_map.txt"
+	// 		err := solutionSvc.Remap(solutionPath, varMapPath)
+	// 		solverSvc.errorSvc.Fatal(err, "Solver: failed to remap variables in the solution")
+	// 	}
 
-		solutionFile, err := os.Open(solutionPath)
-		solverSvc.errorSvc.Fatal(err, "Solver: failed to read the solution")
+	// 	solutionFile, err := os.Open(solutionPath)
+	// 	solverSvc.errorSvc.Fatal(err, "Solver: failed to read the solution")
 
-		verified, err = solutionSvc.Verify(solutionFile, info.Steps)
-		solverSvc.errorSvc.Check(err, "Solver: verification failed")
+	// 	verified, err = solutionSvc.Verify(solutionFile, info.Steps)
+	// 	solverSvc.errorSvc.Check(err, "Solver: verification failed")
 
-		checksum, err = solverSvc.filesystemSvc.Checksum(solutionPath)
-		solverSvc.errorSvc.Fatal(err, "Solver: failed to calculate checksum of the solution "+solutionPath)
-	}
+	// 	checksum, err = solverSvc.filesystemSvc.Checksum(solutionPath)
+	// 	solverSvc.errorSvc.Fatal(err, "Solver: failed to calculate checksum of the solution "+solutionPath)
+	// }
 
-	message := []any{"Solver:", solver_, resultString, exitCode, runtime, encoding}
-	if result == consts.Sat {
-		if verified {
-			message = append(message, "verified")
-		} else {
-			message = append(message, "!verified")
-		}
-	}
-	logrus.Println(message)
+	// message := []any{"Solver:", solver_, resultString, exitCode, runtime, encoding}
+	// if result == solver.Sat {
+	// 	if verified {
+	// 		message = append(message, "verified")
+	// 	} else {
+	// 		message = append(message, "!verified")
+	// 	}
+	// }
+	solverSvc.logSvc.SolverResult(encoding, solver_, exitCode, result, runtime)
 
 	// Store in the database
-	instanceName := strings.TrimSuffix(path.Base(encoding), ".cnf")
-	solutionSvc.Register(encoding, solver_, solver.Solution{
-		Runtime:      runtime,
-		Result:       result,
-		Solver:       solver_,
-		ExitCode:     exitCode,
-		InstanceName: instanceName,
-		Verified:     verified,
-		Checksum:     checksum,
-	})
+	// instanceName := strings.TrimSuffix(path.Base(encoding), ".cnf")
+	// solutionSvc.Register(encoding, solver_, solver.Solution{
+	// 	Runtime:      runtime,
+	// 	Result:       result,
+	// 	Solver:       solver_,
+	// 	ExitCode:     exitCode,
+	// 	InstanceName: instanceName,
+	// 	Verified:     verified,
+	// 	Checksum:     checksum,
+	// })
 }
 
 func (solverSvc *SolverService) Loop(encodingPromises []pipeline.EncodingPromise, parameters pipeline.Solving, handler func(encodingPromise pipeline.EncodingPromise, solver solver.Solver)) {
@@ -190,33 +185,53 @@ func (solverSvc *SolverService) Loop(encodingPromises []pipeline.EncodingPromise
 	}
 }
 
-func (solverSvc *SolverService) ShouldSkip(encoding string, solver_ solver.Solver, timeout int) bool {
-	solutionSvc := solverSvc.solutionSvc
-	errorSvc := solverSvc.errorSvc
+// TODO: Read the log file and see if it actually finished writing
+func (solverSvc *SolverService) ShouldSkip(instancePath string, solver_ solver.Solver, timeout int) bool {
+	logsDir := solverSvc.configSvc.Config.Paths.Logs
+	fileName := path.Base(instancePath) + "." + string(solver_) + ".log"
+	logPath := path.Join(logsDir, fileName)
 
-	solution, err := solutionSvc.Find(encoding, solver_)
-	// Don't skip if there is no solution
-	if err == errorModule.ErrKeyNotFound || err == os.ErrNotExist {
+	result, _, err := solverSvc.ParseOutput(logPath, solver_, false)
+	if err != nil {
 		return false
 	}
 
-	// Handle errors
-	errorSvc.Fatal(err, "Solver: failed to search the solution")
-
-	// Skip solved solutions
-	if err == nil && (solution.Result == consts.Sat || solution.Result == consts.Unsat) {
-		return true
-	}
-
-	// Skip failed solutions: 10 seconds is the threshold
-	if err == nil && solution.Result == consts.Fail && (timeout-int(solution.Runtime.Seconds())) < 10 {
+	if result == solver.Unsat || result == solver.Sat {
 		return true
 	}
 
 	return false
+
+	// solutionSvc := solverSvc.solutionSvc
+	// errorSvc := solverSvc.errorSvc
+
+	// solution, err := solutionSvc.Find(encoding, solver_)
+	// // Don't skip if there is no solution
+	// if err == errorModule.ErrKeyNotFound || err == os.ErrNotExist {
+	// 	return false
+	// }
+
+	// // Handle errors
+	// errorSvc.Fatal(err, "Solver: failed to search the solution")
+
+	// // Skip solved solutions
+	// if err == nil && (solution.Result == solver.Sat || solution.Result == solver.Unsat) {
+	// 	return true
+	// }
+
+	// // Skip failed solutions: 10 seconds is the threshold
+	// if err == nil && solution.Result == solver.Fail && (timeout-int(solution.Runtime.Seconds())) < 10 {
+	// 	return true
+	// }
+
+	// return false
 }
 
 func (solverSvc *SolverService) RunSlurm(previousPipeOutput pipeline.SlurmPipeOutput, parameters pipeline.Solving) pipeline.SlurmPipeOutput {
+	dirs := []string{solverSvc.configSvc.Config.Paths.Solutions, solverSvc.configSvc.Config.Paths.Logs}
+	err := solverSvc.filesystemSvc.PrepareDirs(dirs)
+	solverSvc.errorSvc.Fatal(err, "Solver: failed to prepare directory for storing the solutions and logs")
+
 	slurmSvc := solverSvc.slurmSvc
 	errorSvc := solverSvc.errorSvc
 	config := solverSvc.configSvc.Config
@@ -255,7 +270,7 @@ func (solverSvc *SolverService) RunSlurm(previousPipeOutput pipeline.SlurmPipeOu
 		tasks = append(tasks, task)
 	})
 
-	err := solverSvc.solveSlurmTaskSvc.AddMultiple(tasks)
+	err = solverSvc.solveSlurmTaskSvc.AddMultiple(tasks)
 	errorSvc.Fatal(err, "Solver: failed to add slurm task")
 	logrus.Println("Solver: added", len(tasks), "slurm tasks")
 
@@ -284,14 +299,15 @@ func (solverSvc *SolverService) RunSlurm(previousPipeOutput pipeline.SlurmPipeOu
 }
 
 func (solverSvc *SolverService) RunRegular(encodingPromises []pipeline.EncodingPromise, parameters pipeline.Solving) {
+	dirs := []string{solverSvc.configSvc.Config.Paths.Solutions, solverSvc.configSvc.Config.Paths.Logs}
+	err := solverSvc.filesystemSvc.PrepareDirs(dirs)
+	solverSvc.errorSvc.Fatal(err, "Solver: failed to prepare directory for storing the solutions and logs")
+
 	logrus.Println("Solver: started")
 	pool := pond.New(parameters.Workers, 1000, pond.IdleTimeout(100*time.Millisecond))
 	dependencies := map[string]interface{}{
 		"CubeSelectorService": solverSvc.cubeSelectorSvc,
 	}
-
-	err := solverSvc.filesystemSvc.PrepareTempDir()
-	solverSvc.errorSvc.Fatal(err, "Solver: failed to prepare tmp dir")
 
 	solverSvc.Loop(encodingPromises, parameters, func(encodingPromise pipeline.EncodingPromise, solver_ solver.Solver) {
 		encoding := encodingPromise.Get(dependencies)
