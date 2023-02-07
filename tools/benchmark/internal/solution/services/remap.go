dd20d604d9ba9dc2ed6522dab5c27562e8e2ae62
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2023-02-07 17:09:38 -0500

    Add incomplete method for remapping SatELite-based simplification

---
+++ tools/benchmark/internal/solution/services/remap.go
@@ -0,0 +1,22 @@
+package services
+
+func (solutionSvc *SolutionService) Remap(solutionPath string, varMapPath string) error {
+	// info, err := solutionSvc.encoderSvc.ProcessInstanceName(solutionPath)
+	// if err != nil {
+	// 	return err
+	// }
+	// steps := info.Steps
+
+	// // * 1. Read the reconstruction stack file and determine the literals that need to be preserved
+	// varMapFile, err := os.OpenFile(varMapPath, os.O_RDONLY, 0600)
+	// if err != nil {
+	// 	return err
+	// }
+	// defer varMapFile.Close()
+
+	// replacements := make(map[int]bool)
+	// scanner := bufio.NewScanner(varMapFile)
+
+	// TODO: Implement remapping and verification
+	return nil
+}
