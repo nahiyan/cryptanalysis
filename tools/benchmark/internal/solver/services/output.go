498786daf848ac0dd86c998bf96f18de0a1aa2a9
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2023-02-11 12:11:10 -0500

    Add the solution literal extraction to the solver log parser

--- tools/benchmark/internal/solver/services/output.go
+++ tools/benchmark/internal/solver/services/output.go
@@ -12,26 +12,24 @@ import (
 	"github.com/bitfield/script"
 )
 
-func (solverSvc *SolverService) ParseLog(logPath string, solver_ solver.Solver, extractSolution bool) (solver.Result, time.Duration, error) {
-	// fmt.Println(solver_)
-
+func (solverSvc *SolverService) ParseLog(logPath string, solver_ solver.Solver, solutionLiterals *[]int) (solver.Result, time.Duration, error) {
 	switch solver_ {
 	case solver.Kissat:
-		return parseOutputWith(logPath, "c exit 10", "c exit 20", "c process-time:", 1)
+		return parseOutputWith(logPath, "c exit 10", "c exit 20", "c process-time:", 1, solutionLiterals)
 	case solver.Cadical:
-		return parseOutputWith(logPath, "c exit 10", "c exit 20", "c total process time since initialization:", 1)
+		return parseOutputWith(logPath, "c exit 10", "c exit 20", "c total process time since initialization:", 1, solutionLiterals)
 	case solver.Glucose:
-		return parseOutputWith(logPath, "s SATISFIABLE", "s UNSATISFIABLE", "c CPU time", 1)
+		return parseOutputWith(logPath, "s SATISFIABLE", "s UNSATISFIABLE", "c CPU time", 1, solutionLiterals)
 	case solver.MapleSat:
-		return parseOutputWith(logPath, "SATISFIABLE", "UNSATISFIABLE", "CPU time", 1)
+		return parseOutputWith(logPath, "SATISFIABLE", "UNSATISFIABLE", "CPU time", 1, solutionLiterals)
 	case solver.CryptoMiniSat:
-		return parseOutputWith(logPath, "s SATISFIABLE", "s UNSATISFIABLE", "c time", 0)
+		return parseOutputWith(logPath, "s SATISFIABLE", "s UNSATISFIABLE", "c time", 0, solutionLiterals)
 	}
 
 	return solver.Fail, time.Duration(0), nil
 }
 
-func parseOutputWith(logPath, satText, unsatText, processTimeText string, processTimeFieldOffset int) (solver.Result, time.Duration, error) {
+func parseOutputWith(logPath, satText, unsatText, processTimeText string, processTimeFieldOffset int, solutionLiterals *[]int) (solver.Result, time.Duration, error) {
 	processTime := time.Duration(0)
 	result := solver.Result(solver.Fail)
 
@@ -61,7 +59,31 @@ func parseOutputWith(logPath, satText, unsatText, processTimeText string, proces
 		result = solver.Fail
 	}
 
-	// fmt.Println(processTime, result, seconds)
+	// Extract the solution literals
+	if solutionLiterals != nil {
+		lines, err := script.File(logPath).Match("v ").Slice()
+		if err != nil {
+			return result, processTime, err
+		}
+
+		for _, line := range lines {
+			if !strings.HasPrefix(line, "v") {
+				continue
+			}
+			segments := strings.Fields(line)
+			for _, segment := range segments {
+				if segment == "0" || segment == "v" {
+					continue
+				}
+
+				literal, err := strconv.Atoi(segment)
+				if err != nil {
+					return result, processTime, err
+				}
+				*solutionLiterals = append(*solutionLiterals, literal)
+			}
+		}
+	}
 
 	return result, processTime, nil
 }
