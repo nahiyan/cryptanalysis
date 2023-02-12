acbf21d5cb495d65bad3916415aa79fbc9abf269
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2023-02-12 18:22:20 -0500

    Add binary cube encoder and decoder

--- tools/benchmark/internal/cubeset/services/service.go
+++ tools/benchmark/internal/cubeset/services/service.go
@@ -1,46 +1,118 @@
 package services
 
 import (
-	"benchmark/internal/cubeset"
-	"time"
+	"bufio"
+	"encoding/binary"
+	"os"
+	"strconv"
+	"strings"
 )
 
-type Properties struct {
-	Bucket string
-}
-
-func (cubesetSvc *CubesetService) Init() {
-	cubesetSvc.Bucket = "cubesets"
-}
-
-func (cubesetSvc *CubesetService) Register(cubesetFilePath string, cubeSet cubeset.CubeSet) error {
-	checksum, err := cubesetSvc.filesystemSvc.Checksum(cubesetFilePath)
+func (cubesetSvc *CubesetService) BinEncode(cubesetPath string) error {
+	cubesFile, err := os.Open(cubesetPath)
 	if err != nil {
 		return err
 	}
+	defer cubesFile.Close()
 
-	data, err := cubesetSvc.marshallingSvc.BinEncode(cubeSet)
+	binCubesMapFile, err := os.OpenFile(cubesetPath+".bcubes.map", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
+	defer binCubesMapFile.Close()
 
-	startTime := time.Now()
-	defer cubesetSvc.filesystemSvc.LogInfo("Cubeset: stored", cubesetFilePath, checksum, time.Since(startTime).String())
+	binCubesFile, err := os.OpenFile(cubesetPath+".bcubes", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer binCubesFile.Close()
 
-	err = cubesetSvc.databaseSvc.Set(cubesetSvc.Bucket, []byte(checksum), data)
-	return err
-}
-
-func (cubesetSvc *CubesetService) All() ([]cubeset.CubeSet, error) {
-	cubesets := []cubeset.CubeSet{}
-	cubesetSvc.databaseSvc.All(cubesetSvc.Bucket, func(key, value []byte) {
-		var cubeset cubeset.CubeSet
-		if err := cubesetSvc.marshallingSvc.BinDecode(value, &cubeset); err != nil {
-			return
+	scanner := bufio.NewScanner(cubesFile)
+	addressAccumulator := uint32(0)
+	binCubesMapWriter := bufio.NewWriter(binCubesMapFile)
+	binCubesWriter := bufio.NewWriter(binCubesFile)
+	for scanner.Scan() {
+		// Bin. Cube
+		line := scanner.Text()
+		literals := strings.Fields(line[2 : len(line)-2])
+		for _, literal_ := range literals {
+			literal, err := strconv.ParseInt(literal_, 10, 16)
+			if err != nil {
+				return err
+			}
+			bytes := make([]byte, 2)
+			binary.BigEndian.PutUint16(bytes, uint16(literal))
+			_, err = binCubesWriter.Write(bytes)
+			if err != nil {
+				return err
+			}
 		}
 
-		cubesets = append(cubesets, cubeset)
-	})
+		// Line map
+		{
+			literalsCount := uint32(len(literals))
+			address := addressAccumulator + literalsCount
+			addressAccumulator = address
 
-	return cubesets, nil
+			bytes := make([]byte, 4)
+			binary.BigEndian.PutUint32(bytes, address)
+			_, err := binCubesMapWriter.Write(bytes)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	binCubesWriter.Flush()
+	binCubesMapWriter.Flush()
+
+	return nil
+}
+
+func (cubesetSvc *CubesetService) GetCube(cubesetPath string, index int) ([]int, error) {
+	cubesFile, err := os.OpenFile(cubesetPath+".bcubes", os.O_RDONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	defer cubesFile.Close()
+
+	mapFile, err := os.OpenFile(cubesetPath+".bcubes.map", os.O_RDONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	defer mapFile.Close()
+
+	// Every index in the map file is the index of the word in the bcubes file, and every word in the bcubes file is 16 bits in size
+	endIndex_ := make([]byte, 4)
+	_, err = mapFile.ReadAt(endIndex_, int64((index-1)*4))
+	if err != nil {
+		return nil, err
+	}
+	endIndex := binary.BigEndian.Uint32(endIndex_)
+	endAddress := int64(endIndex * 2)
+
+	startIndex := uint32(0)
+	if index > 1 {
+		startAddress_ := make([]byte, 4)
+		_, err := mapFile.ReadAt(startAddress_, int64(index-2)*4)
+		if err != nil {
+			return nil, err
+		}
+		startIndex = binary.BigEndian.Uint32(startAddress_)
+	}
+	startAddress := int64(startIndex * 2)
+
+	bytes := make([]byte, endAddress-startAddress)
+	_, err = cubesFile.ReadAt(bytes, startAddress)
+	if err != nil {
+		return nil, err
+	}
+
+	cube := []int{}
+	for i := 0; i < len(bytes); i += 2 {
+		word := bytes[i : i+2]
+		literal := int16(binary.BigEndian.Uint16(word))
+		cube = append(cube, int(literal))
+	}
+
+	return cube, nil
 }
