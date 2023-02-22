64f43ba67982f4e0c9304204460e22e2bb2be9f7
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2023-02-22 16:22:33 -0500

    Add functionality of returning the runtime from the solver logs

--- tools/benchmark/internal/solver/services/output.go
+++ tools/benchmark/internal/solver/services/output.go
@@ -12,7 +12,7 @@ import (
 	"github.com/bitfield/script"
 )
 
-func (solverSvc *SolverService) ParseLog(logPath string, solver_ solver.Solver, solutionLiterals *[]int) (solver.Result, time.Duration, error) {
+func (solverSvc *SolverService) ParseLog(logPath string, solver_ solver.Solver, solutionLiterals *[]int) (solver.Result, time.Duration, time.Duration, error) {
 	switch solver_ {
 	case solver.Kissat:
 		return parseOutputWith(logPath, "c exit 10", "c exit 20", "c process-time:", 1, solutionLiterals)
@@ -26,31 +26,50 @@ func (solverSvc *SolverService) ParseLog(logPath string, solver_ solver.Solver, 
 		return parseOutputWith(logPath, "s SATISFIABLE", "s UNSATISFIABLE", "c time", 0, solutionLiterals)
 	}
 
-	return solver.Fail, time.Duration(0), nil
+	return solver.Fail, time.Duration(0), time.Duration(0), nil
 }
 
-func parseOutputWith(logPath, satText, unsatText, processTimeText string, processTimeFieldOffset int, solutionLiterals *[]int) (solver.Result, time.Duration, error) {
+func parseOutputWith(logPath, satText, unsatText, processTimeText string, processTimeFieldOffset int, solutionLiterals *[]int) (solver.Result, time.Duration, time.Duration, error) {
 	processTime := time.Duration(0)
+	runTime := time.Duration(0)
 	result := solver.Result(solver.Fail)
 
-	regexp := regexp.MustCompile(fmt.Sprintf("(%s)|(%s)|(%s)", processTimeText, satText, unsatText))
-
-	output, err := script.File(logPath).MatchRegexp(regexp).Slice()
+	regexp := regexp.MustCompile(fmt.Sprintf("(%s)|(%s)|(%s)|(Info: Ended after)", processTimeText, satText, unsatText))
+	matches, err := script.File(logPath).MatchRegexp(regexp).Slice()
 	if err != nil {
-		return result, processTime, err
+		return result, processTime, runTime, err
 	}
 
-	if len(output) != 2 {
-		return result, processTime, errors.New("invalid solver output format")
+	// If none of the expected matches are met
+	if len(matches) == 0 {
+		return result, processTime, runTime, errors.New("invalid solver output format")
+	}
+
+	// Parse runtime
+	for _, match := range matches {
+		if strings.HasPrefix(match, "Info: Ended after") {
+			pieces := strings.Fields(match)
+			runTime_, err := strconv.ParseFloat(pieces[len(pieces)-2], 64)
+			if err != nil {
+				return result, processTime, runTime, errors.New("failed to parse runtime")
+			}
+			runTime = time.Duration(runTime_*1000) * time.Millisecond
+			break
+		}
+	}
+
+	// See if it failed
+	if len(matches) != 3 {
+		return result, processTime, runTime, nil
 	}
 
 	// Process the time it took to process the instance
-	processTimeFields := strings.Fields(output[0])
+	processTimeFields := strings.Fields(matches[0])
 	seconds, _ := strconv.ParseFloat(processTimeFields[len(processTimeFields)-(processTimeFieldOffset+1)], 64)
 	processTime = time.Duration(seconds*1000) * time.Millisecond
 
 	// Process the result
-	switch output[1] {
+	switch matches[1] {
 	case satText:
 		result = solver.Sat
 	case unsatText:
@@ -63,7 +82,7 @@ func parseOutputWith(logPath, satText, unsatText, processTimeText string, proces
 	if solutionLiterals != nil {
 		lines, err := script.File(logPath).Match("v ").Slice()
 		if err != nil {
-			return result, processTime, err
+			return result, processTime, runTime, err
 		}
 
 		for _, line := range lines {
@@ -78,12 +97,12 @@ func parseOutputWith(logPath, satText, unsatText, processTimeText string, proces
 
 				literal, err := strconv.Atoi(segment)
 				if err != nil {
-					return result, processTime, err
+					return result, processTime, runTime, err
 				}
 				*solutionLiterals = append(*solutionLiterals, literal)
 			}
 		}
 	}
 
-	return result, processTime, nil
+	return result, processTime, runTime, nil
 }
