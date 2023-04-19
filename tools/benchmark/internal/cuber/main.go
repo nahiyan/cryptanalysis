7c22186a3f70325a0e7f1bd6dd35b6a0f8a3369c
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2023-04-19 15:39:24 -0400

    Add threshold type specifier for the cuber

--- tools/benchmark/internal/cuber/main.go
+++ tools/benchmark/internal/cuber/main.go
@@ -5,3 +5,10 @@ import "errors"
 var (
 	ErrCubesetViolatedConstraints = errors.New("cubeset violated constraints")
 )
+
+type ThresholdType string
+
+const (
+	CutoffVars  = "cutoff_vars"
+	CutoffDepth = "cutoff_depth"
+)
