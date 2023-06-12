f37b4b67be23fc8d071cbd6269ce5aa4853dce1b
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2023-06-12 12:24:01 -0400

    Remove comments in the SHA256 test suite

--- internal/sha256/services/sha256_test.go
+++ internal/sha256/services/sha256_test.go
@@ -17,16 +17,3 @@ func TestSha256Case1(t *testing.T) {
 		t.Fatalf("got hash = %s but expected 73703799050cdca89b07dd7605078b469b6e3608d6363ddf8d97bffe009dbc6f\n", digest)
 	}
 }
-
-// func TestSha256Case2(t *testing.T) {
-// 	sha256Svc := Sha256Service{}
-// 	bytes, _ := hex.DecodeString("dfe6feebc4437a8511af5182e3b13f035103e1fcea231da2c3b513d1b95fa9d77a2a331c2ddf2607699a2daec1827561fe80aeedcf45b09a5b596c8fd0265347")
-// 	digest, err := sha256Svc.Run(bytes, 28, false)
-// 	if err != nil {
-// 		t.Fatal("failed to compute MD5 hash: ", err)
-// 	}
-
-// 	if digest != "ffffffffffffffffffffffffffffffff" {
-// 		t.Fatalf("got hash = %s but expected all-one-bit hash\n", digest)
-// 	}
-// }
