0ca42efb4f75432c23cc6d7037d6621ac3096e27
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2024-01-14 21:18:30 -0500

    Fix differences for the K variables

--- encoders/nejati-collision/main.cpp
+++ encoders/nejati-collision/main.cpp
@@ -25,6 +25,20 @@ int cfg_rand_inp_diff;
 string cfg_diff_const_file;
 Rules prop_rules;
 
+unsigned rnd_const[] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1,
+    0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786,
+    0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
+    0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b,
+    0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a,
+    0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+};
+
 void load_rules(string filePath)
 {
     // Open the file
@@ -73,15 +87,15 @@ void fix_starting_point(SHA256& block, char& diff, int* dx, int* x, int* x_)
     auto& formula = block.cnf;
     switch (diff) {
     case '-':
-        formula.fixedValue(dx + 0, 1, 1);
+        // formula.fixedValue(dx + 0, 1, 1);
         formula.fixedValue(dx + 1, 0, 1);
         formula.fixedValue(dx + 2, 0, 1);
-        formula.fixedValue(dx + 3, 1, 1);
+        // formula.fixedValue(dx + 3, 1, 1);
         break;
     case 'x':
         formula.fixedValue(dx + 0, 0, 1);
-        formula.fixedValue(dx + 1, 1, 1);
-        formula.fixedValue(dx + 2, 1, 1);
+        // formula.fixedValue(dx + 1, 1, 1);
+        // formula.fixedValue(dx + 2, 1, 1);
         formula.fixedValue(dx + 3, 0, 1);
         break;
     case '0':
@@ -357,7 +371,20 @@ void collision(int rounds)
         g.cnf.basic_rules(DT[i], f.T[i], g.T[i]);
         g.cnf.newDiff(DK[i], "DK_" + to_string(i));
         // Fix the difference
-        g.cnf.fixedDiff(DK[i], { 1, 0, 0, 1 });
+        for (int j = 0; j < 32; j++) {
+            bool is_true = rnd_const[i] >> j & 1;
+            if (is_true) {
+                g.cnf.addClause({ -DK[i][j][0] });
+                g.cnf.addClause({ -DK[i][j][1] });
+                g.cnf.addClause({ -DK[i][j][2] });
+                g.cnf.addClause({ DK[i][j][3] });
+            } else {
+                g.cnf.addClause({ DK[i][j][0] });
+                g.cnf.addClause({ -DK[i][j][1] });
+                g.cnf.addClause({ -DK[i][j][2] });
+                g.cnf.addClause({ -DK[i][j][3] });
+            }
+        }
         g.cnf.diff_add(prop_rules, DT[i], DE[i], Dsigma1[i], Dr0carry[i], Dr0Carry[i], Df1[i], DK[i], DW[i]);
 
         // Addition: E[i+4] = A[i] + T
