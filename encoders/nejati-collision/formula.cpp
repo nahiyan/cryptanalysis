6aa52b1e363710d905eecbbc296f0063e0cbe483
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2024-01-14 21:16:50 -0500

    Reformat formula.cpp

--- encoders/nejati-collision/formula.cpp
+++ encoders/nejati-collision/formula.cpp
@@ -80,15 +80,6 @@ void Formula::fixedValue(int* z, unsigned value, int n)
         addClause({ x });
     }
 }
-void Formula::fixedDiff(int z[32][4], vector<int> value)
-{
-    for (int i = 0; i < 32; i++) {
-        vector<int> clause;
-        for (int j = 0; j < value.size(); j++)
-            clause.push_back((value[j] == 1 ? -1 : 1) * z[i][j]);
-        addClause(clause);
-    }
-}
 
 void Formula::rotl(int* z, int* x, int p, int n)
 {
