762caeb80e15952675e94e142132b9b895b38df1
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2024-01-22 00:28:47 -0500

    Add support for blocking clauses in the clause verifier

--- clause-verifier/main.py
+++ clause-verifier/main.py
@@ -3,14 +3,25 @@ import subprocess
 
 
 def get_solution(file_path):
+    assignment = {}
+
     try:
         # Run the grep command and capture the output
         result = subprocess.run(
             ["grep", "^v.*", file_path], stdout=subprocess.PIPE, text=True, check=True
         )
 
-        # Print the output
-        return result.stdout
+        lines = result.stdout[:-3].split("\n")
+        for line in lines:
+            lits = line[2:].split(" ")
+            for lit_ in lits:
+                if lit_[:1] == "v" or len(lit_.strip()) == 0:
+                    continue
+                lit = int(lit_)
+                assert lit != 0
+                assignment[abs(lit)] = True if lit > 0 else False
+
+        return assignment
 
     except subprocess.CalledProcessError as e:
         # Handle errors if the grep command fails
@@ -21,62 +32,69 @@ def get_clauses(file_path):
     try:
         # Run the grep command and capture the output
         result = subprocess.run(
-            ["grep", "-E", "^Antecedent.*|^Asked for reason of.*", file_path],
+            [
+                "grep",
+                "-E",
+                "^Reason clause:.*|^Blocking clause:.*",
+                file_path,
+            ],
             stdout=subprocess.PIPE,
             text=True,
             check=True,
         )
 
+        lines = result.stdout.split("\n")
+        clauses = []
+        for line_ in lines:
+            clause_type = "reason" if line_.startswith("Reason clause:") else "blocking" if line_.startswith("Blocking clause:") else ""
+            if clause_type == "":
+                continue
+            line = line_.replace("Reason clause: ", "").replace("Blocking clause: ", "").strip()
+            lits = [int(lit_) for lit_ in line.split(" ")]
+            clause = {"lits": lits, "type": clause_type}
+            clauses.append(clause)
+
         # Print the output
-        return result.stdout
+        return clauses
 
     except subprocess.CalledProcessError as e:
         # Handle errors if the grep command fails
         print(f"Error: {e}")
+        return []
+
+def is_satisfied(clause, assignment):
+    for lit in clause:
+        var = abs(lit)
+        value = True if lit > 0 else False
+        if assignment[var] == value:
+            return True
+    return False
+
+def print_clause(clause, assignment):
+    print("Reason: " if clause["type"] == "reason" else "Blocking: ", end="")
+    for lit in clause["lits"]:
+        var = abs(lit)
+        value = True if lit > 0 else False
+        print(str(lit) + "(" + ("✓" if assignment[var] == value else "𐄂") + ")", end=" ")
+    print()
+
+def verify_clauses(sol_log_path, solver_log_path):
+    assignment = get_solution(sol_log_path)
+    clauses = get_clauses(solver_log_path)
+    stats = {"reason": 0, "blocking": 0}
+    for clause in clauses:
+        lits = clause["lits"]
+        type_ = clause["type"]
+        stats[type_] += 1
+        if type_ == "reason":
+            if not is_satisfied(lits, assignment):
+                print_clause(clause, assignment)
+        elif type_ == "blocking":
+            if not is_satisfied(lits, assignment):
+                print_clause(clause, assignment)
+    print(stats)
 
 
 sol_log_path = sys.argv[1]
 solver_log_path = sys.argv[2]
-
-assignment = {}
-lines = get_solution(sol_log_path)[:-3].split("\n")
-for line in lines:
-    lits = line[2:].split(" ")
-    for lit_ in lits:
-        if lit_[:1] == "v" or len(lit_.strip()) == 0:
-            continue
-        lit = int(lit_)
-        assert lit != 0
-        assignment[abs(lit)] = True if lit > 0 else False
-
-antecedents_count = 0
-p_lit = 0
-lines = get_clauses(solver_log_path).split("\n")
-for line in lines:
-    if line.startswith("Antecedent:"):
-        if p_lit == 0:
-            continue
-        antecedents_count += 1
-        # assert p_lit != 0
-        antecedent = line[12:].strip()
-        reason_clause = [p_lit]
-        lits = antecedent.split(" ")
-        for lit in lits:
-            reason_clause.append(int(lit))
-
-        is_satisfied = False
-        for lit in reason_clause:
-            var = abs(lit)
-            value = True if lit > 0 else False
-            if assignment[var] == value:
-                is_satisfied = True
-        if not is_satisfied:
-            print("Clause: ", end="")
-            for lit in reason_clause:
-                print(lit, end=" ")
-            print()
-        p_lit = 0
-    elif line.startswith("Asked for reason of"):
-        p_lit = int(line.split(" ")[4])
-
-print(antecedents_count, "reason clauses")
+verify_clauses(sol_log_path, solver_log_path)
